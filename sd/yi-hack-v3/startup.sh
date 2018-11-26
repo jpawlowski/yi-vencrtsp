c65d7230217cba2ee7616edacde663fefdd5db23
Julian Pawlowski <julian@pawlowski.me>
2018-11-26 19:17:10 +0100

    Refactoring

--- sd/yi-hack-v3/startup.sh
+++ sd/yi-hack-v3/startup.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+HOMEVER=`cat /home/homever`
 
-# Allow to modify or restore yi init file
-if [[ -f /tmp/sd/yi-hack-v3/yi_init.sh ]]; then
-    cp /tmp/sd/yi-hack-v3/yi_init.sh /home/app/init.sh
+# Replace init.sh
+if [[ ! -f /home/app/init.sh.$HOMEVER ]] &&
+   [[ -f /tmp/sd/yi-hack-v3/sbin/yi_init.sh ]] &&
+   [[ -d /tmp/sd/yi-hack-v3/lib/hi3518e ]]; then
+      cp /home/app/init.sh /home/app/init.sh.$HOMEVER
+      cp /tmp/sd/yi-hack-v3/sbin/yi_init.sh /home/app/init.sh
+      reboot
 fi
-
-
-
-
